c44ee73ed0329a35a7e545141c51fcea2ec5ff19
tobtoht <thotbot@protonmail.com>
2020-11-24 14:40:17 +0100

    PasswordDialog: show warning if password incorrect

---
+++ src/dialog/passworddialog.cpp
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2020, The Monero Project.
+
+#include "passworddialog.h"
+#include "ui_passworddialog.h"
+
+PasswordDialog::PasswordDialog(QWidget *parent, const QString &walletName, bool incorrectPassword)
+        : QDialog(parent)
+        , ui(new Ui::PasswordDialog)
+{
+    ui->setupUi(this);
+
+    ui->label_wallet->setText(QString("Please enter password for wallet: %1").arg(walletName));
+    ui->label_incorrectPassword->setVisible(incorrectPassword);
+
+    connect(ui->buttonBox, &QDialogButtonBox::accepted, [this]{
+        password = ui->line_password->text();
+    });
+
+    this->adjustSize();
+}
+
+PasswordDialog::~PasswordDialog()
+{
+    delete ui;
+}
