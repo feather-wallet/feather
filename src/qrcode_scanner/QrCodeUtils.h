5175ffd8a540b31f4978557f3ab5d9b077ae5f77
tobtoht <thotbot@protonmail.com>
2021-07-08 22:21:18 +0200

    QrCodeUtils

---
+++ src/qrcode_scanner/QrCodeUtils.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2020-2021, The Monero Project.
+
+#ifndef FEATHER_QRCODEUTILS_H
+#define FEATHER_QRCODEUTILS_H
+
+#include <QImage>
+#include <zbar.h>
+
+class QrCodeUtils {
+public:
+    static bool zimageFromQImage(const QImage &qImg, zbar::Image &dst);
+    static QString scanImage(const QImage &img);
+};
+
+
+#endif //FEATHER_QRCODEUTILS_H
