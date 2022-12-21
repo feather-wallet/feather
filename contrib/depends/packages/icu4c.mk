31ead39944028a66ce568767e5c407b3b533ab8b
tobtoht <tob@featherwallet.org>
2022-12-21 23:36:19 +0100

    depends: icu4c: no timestamp in makefile

--- contrib/depends/packages/icu4c.mk
+++ contrib/depends/packages/icu4c.mk
@@ -3,14 +3,15 @@ $(package)_version=55.2
 $(package)_download_path=https://github.com/unicode-org/icu/releases/download/release-55-2/
 $(package)_file_name=$(package)-55_2-src.tgz
 $(package)_sha256_hash=eda2aa9f9c787748a2e2d310590720ca8bcc6252adf6b4cfb03b65bef9d66759
-$(package)_patches=icu-001-dont-build-static-dynamic-twice.patch
+$(package)_patches=icu-001-dont-build-static-dynamic-twice.patch no-timestamp-in-makefile.patch
 
 define $(package)_set_vars
   $(package)_build_opts=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -DU_USING_ICU_NAMESPACE=0 -DU_STATIC_IMPLEMENTATION -DU_COMBINED_IMPLEMENTATION -fPIC -DENABLE_STATIC=YES -DPGKDATA_MODE=static"
 endef
 
 define $(package)_config_cmds
-  patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch &&\
+  patch -p1 < $($(package)_patch_dir)/icu-001-dont-build-static-dynamic-twice.patch && \
+  patch -p1 < $($(package)_patch_dir)/no-timestamp-in-makefile.patch && \
   mkdir builda &&\
   mkdir buildb &&\
   cd builda &&\
