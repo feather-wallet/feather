2b767c0bc972491d3e95bfbf476c9bea76839909
tobtoht <tob@featherwallet.org>
2022-12-25 21:59:05 +0100

    depends: native_cctools: no build date

--- contrib/depends/packages/native_cctools.mk
+++ contrib/depends/packages/native_cctools.mk
@@ -5,6 +5,7 @@ $(package)_file_name=$($(package)_version).tar.gz
 $(package)_sha256_hash=6b73269efdf5c58a070e7357b66ee760501388549d6a12b423723f45888b074b
 $(package)_build_subdir=cctools
 $(package)_dependencies=native_libtapi
+$(package)_patches=no-build-date.patch
 
 define $(package)_set_vars
   $(package)_config_opts=--target=$(host)
@@ -17,6 +18,7 @@ define $(package)_set_vars
 endef
 
 define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/no-build-date.patch && \
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub cctools
 endef
 
