64f425badf1df05ed474bd55f7b57001bacb0cd5
tobtoht <tob@featherwallet.org>
2023-01-02 20:30:11 +0100

    update copyright for 2023

--- src/libwalletqt/PassphraseHelper.h
+++ src/libwalletqt/PassphraseHelper.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// SPDX-FileCopyrightText: 2014-2022 The Monero Project
+// SPDX-FileCopyrightText: 2020-2023 The Monero Project
 
 #ifndef MONERO_GUI_PASSPHRASEHELPER_H
 #define MONERO_GUI_PASSPHRASEHELPER_H
