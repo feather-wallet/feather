b057f750a5ed43d44e860dfff4d1e31d6e139f8a
tobtoht <tob@featherwallet.org>
2023-02-28 12:33:32 +0100

    depends: bump polyseed to 2.0.0

--- contrib/depends/packages/polyseed.mk
+++ contrib/depends/packages/polyseed.mk
@@ -1,15 +1,14 @@
 package=polyseed
-$(package)_version=1.0.0
+$(package)_version=2.0.0
 $(package)_download_path=https://github.com/tevador/polyseed/archive/refs/tags/
 $(package)_file_name=v$($(package)_version).tar.gz
 $(package)_sha256_hash=45f1e6c08575286581079e6e26d341a3a33abe1f1ee2d026bd098cf632ea2349
 $(package)_mingw32_dependencies=native_cmake
-$(package)_patches=no_shared.patch force-static-mingw.patch 32-bit.patch
+$(package)_patches=force-static-mingw.patch no_time_t.patch
 
 define $(package)_preprocess_cmds
-    patch -p1 < $($(package)_patch_dir)/no_shared.patch && \
     patch -p1 < $($(package)_patch_dir)/force-static-mingw.patch && \
-    patch -p1 < $($(package)_patch_dir)/32-bit.patch
+    patch -p1 < $($(package)_patch_dir)/no_time_t.patch
 endef
 
 define $(package)_config_cmds
