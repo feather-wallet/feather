9fd4b141a1ab650ef272dfc3c35d282f528cb2b8
tobtoht <tob@featherwallet.org>
2023-04-05 11:03:44 +0200

    depends: zstd: bump to 1.5.5

--- contrib/depends/packages/zstd.mk
+++ contrib/depends/packages/zstd.mk
@@ -1,8 +1,8 @@
 package=zstd
-$(package)_version=1.5.4
+$(package)_version=1.5.5
 $(package)_download_path=https://github.com/facebook/zstd/releases/download/v$($(package)_version)
 $(package)_file_name=zstd-$($(package)_version).tar.gz
-$(package)_sha256_hash=0f470992aedad543126d06efab344dc5f3e171893810455787d38347343a4424
+$(package)_sha256_hash=9c4396cc829cfae319a6e2615202e82aad41372073482fce286fac78646d3ee4
 
 define $(package)_build_cmds
   $($(package)_cmake) -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DHOST=$(host) -DZSTD_LEGACY_SUPPORT=OFF -B build-cmake-debug -S build/cmake && \
