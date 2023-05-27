b8845e02ca16406a141988fcbb5e65b063049035
tobtoht <tob@featherwallet.org>
2023-05-27 16:49:11 +0200

    depends: update hidapi to 0.14.0

--- contrib/depends/packages/hidapi.mk
+++ contrib/depends/packages/hidapi.mk
@@ -1,24 +1,18 @@
 package=hidapi
-$(package)_version=0.13.0
+$(package)_version=0.14.0
 $(package)_download_path=https://github.com/libusb/hidapi/archive/refs/tags
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=e35eabe4ad59bd6e24dce6136f084997cdecd9bb7f6e83b40f3cc15b0ea8d56f
+$(package)_sha256_hash=a5714234abe6e1f53647dd8cba7d69f65f71c558b7896ed218864ffcf405bcbd
 $(package)_linux_dependencies=libusb eudev
-$(package)_patches=missing_win_include.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--enable-static --disable-shared
 $(package)_config_opts+=--prefix=$(host_prefix)
-#$(package)_config_opts_darwin+=RANLIB="$(host_prefix)/native/bin/x86_64-apple-darwin-ranlib" AR="$(host_prefix)/native/bin/x86_64-apple-darwin-ar" CC="$(host_prefix)/native/bin/$($(package)_cc)"
 $(package)_config_opts_linux+=libudev_LIBS="-L$(host_prefix)/lib -ludev"
 $(package)_config_opts_linux+=libudev_CFLAGS=-I$(host_prefix)/include
 $(package)_config_opts_linux+=libusb_LIBS="-L$(host_prefix)/lib -lusb-1.0"
 $(package)_config_opts_linux+=libusb_CFLAGS=-I$(host_prefix)/include/libusb-1.0
 $(package)_config_opts_linux+=--with-pic
-endef
-
-define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/missing_win_include.patch
 endef
 
 define $(package)_config_cmds
@@ -37,4 +31,3 @@ endef
 define $(package)_postprocess_cmds
   rm lib/*.la
 endef
-
