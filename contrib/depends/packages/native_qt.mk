bfc50785c04e39124ef8e6b91194f7cc6b596b62
tobtoht <tob@featherwallet.org>
2023-07-02 18:03:39 +0200

    depends: native_qt: cleanup

--- contrib/depends/packages/native_qt.mk
+++ contrib/depends/packages/native_qt.mk
@@ -8,8 +8,6 @@ $(package)_qt_libs=corelib network widgets gui plugins testlib
 $(package)_patches  = dont_hardcode_pwd.patch
 $(package)_patches += fast_fixed_dtoa_no_optimize.patch
 $(package)_patches += guix_cross_lib_path.patch
-$(package)_patches += mac-qmake.conf
-$(package)_patches += no_pthread_cond_clockwait.patch
 $(package)_patches += qtbase-moc-ignore-gcc-macro.patch
 $(package)_patches += rcc_hardcode_timestamp.patch
 $(package)_patches += root_CMakeLists.txt
@@ -38,90 +36,83 @@ $(package)_config_opts_debug += -optimized-tools
 $(package)_config_opts += -libexecdir $(build_prefix)/qt-host/bin
 $(package)_config_opts += -c++std c++17
 $(package)_config_opts += -confirm-license
+$(package)_config_opts += -opensource
+$(package)_config_opts += -pkg-config
+$(package)_config_opts += -prefix $(build_prefix)/qt-host
+$(package)_config_opts += -static
+$(package)_config_opts += -platform linux-g++ -xplatform linux-g++
+ifneq (,$(findstring -stdlib=libc++,$($(1)_cxx)))
+$(package)_config_opts_x86_64 = -xplatform linux-clang-libc++
+endif
+
 $(package)_config_opts += -no-cups
 $(package)_config_opts += -no-egl
 $(package)_config_opts += -no-eglfs
 $(package)_config_opts += -no-evdev
+$(package)_config_opts += -no-feature-androiddeployqt
+$(package)_config_opts += -no-feature-colordialog
+$(package)_config_opts += -no-feature-dbus
+$(package)_config_opts += -no-feature-dial
+$(package)_config_opts += -no-feature-fontcombobox
+$(package)_config_opts += -no-feature-fontconfig
+$(package)_config_opts += -no-feature-freetype
+$(package)_config_opts += -no-feature-harfbuzz
+$(package)_config_opts += -no-feature-http
+$(package)_config_opts += -no-feature-image_heuristic_mask
+$(package)_config_opts += -no-feature-keysequenceedit
+$(package)_config_opts += -no-feature-lcdnumber
+$(package)_config_opts += -no-feature-networkdiskcache
+$(package)_config_opts += -no-feature-pdf
+$(package)_config_opts += -no-feature-png
+$(package)_config_opts += -no-feature-printdialog
+$(package)_config_opts += -no-feature-printer
+$(package)_config_opts += -no-feature-printpreviewdialog
+$(package)_config_opts += -no-feature-printpreviewwidget
+$(package)_config_opts += -no-feature-qmake
+$(package)_config_opts += -no-feature-sessionmanager
+$(package)_config_opts += -no-feature-spatialaudio
+$(package)_config_opts += -no-feature-sql
+$(package)_config_opts += -no-feature-syntaxhighlighter
+$(package)_config_opts += -no-feature-testlib
+$(package)_config_opts += -no-feature-textmarkdownwriter
+$(package)_config_opts += -no-feature-textodfwriter
+$(package)_config_opts += -no-feature-topleveldomain
+$(package)_config_opts += -no-feature-undocommand
+$(package)_config_opts += -no-feature-undogroup
+$(package)_config_opts += -no-feature-undostack
+$(package)_config_opts += -no-feature-undoview
+$(package)_config_opts += -no-feature-vnc
+$(package)_config_opts += -no-feature-vulkan
+$(package)_config_opts += -no-feature-widgets
+$(package)_config_opts += -no-feature-xkbcommon
+$(package)_config_opts += -no-feature-xlib
 $(package)_config_opts += -no-gif
 $(package)_config_opts += -no-glib
-$(package)_config_opts += -no-icu
 $(package)_config_opts += -no-ico
+$(package)_config_opts += -no-icu
 $(package)_config_opts += -no-kms
-$(package)_config_opts += -no-linuxfb
-#$(package)_config_opts += -no-libjpeg # Needed
-#$(package)_config_opts += -no-libproxy # Needed
+$(package)_config_opts += -no-libjpeg
 $(package)_config_opts += -no-libudev
+$(package)_config_opts += -no-linuxfb
+$(package)_config_opts += -nomake examples
+$(package)_config_opts += -nomake tests
 $(package)_config_opts += -no-mtdev
+$(package)_config_opts += -no-opengl
 $(package)_config_opts += -no-openssl
-#$(package)_config_opts += -openssl-linked
 $(package)_config_opts += -no-openvg
+$(package)_config_opts += -no-pch
 $(package)_config_opts += -no-reduce-relocations
 $(package)_config_opts += -no-schannel
 $(package)_config_opts += -no-sctp
 $(package)_config_opts += -no-securetransport
 $(package)_config_opts += -no-system-proxies
 $(package)_config_opts += -no-use-gold-linker
-$(package)_config_opts += -no-zstd
-$(package)_config_opts += -nomake examples
-$(package)_config_opts += -nomake tests
-$(package)_config_opts += -opensource
-$(package)_config_opts += -pkg-config
-$(package)_config_opts += -prefix $(build_prefix)/qt-host
-$(package)_config_opts += -qt-libpng
-$(package)_config_opts += -qt-pcre
-$(package)_config_opts += -qt-harfbuzz
-$(package)_config_opts += -qt-zlib
-$(package)_config_opts += -static
-$(package)_config_opts += -no-feature-colordialog
-#$(package)_config_opts += -no-feature-concurrent # Needed
-$(package)_config_opts += -no-feature-dial
-$(package)_config_opts += -no-feature-fontcombobox
-$(package)_config_opts += -no-feature-http
-$(package)_config_opts += -no-feature-image_heuristic_mask
-$(package)_config_opts += -no-feature-keysequenceedit
-$(package)_config_opts += -no-feature-lcdnumber
-$(package)_config_opts += -no-feature-networkdiskcache
-#$(package)_config_opts += -no-feature-networkproxy # Needed
-$(package)_config_opts += -no-feature-pdf
-$(package)_config_opts += -no-feature-printdialog
-$(package)_config_opts += -no-feature-printer
-$(package)_config_opts += -no-feature-printpreviewdialog
-$(package)_config_opts += -no-feature-printpreviewwidget
-$(package)_config_opts += -no-feature-sessionmanager
-#$(package)_config_opts += -no-feature-socks5 # Needed
-$(package)_config_opts += -no-feature-spatialaudio
-$(package)_config_opts += -no-feature-sql
-$(package)_config_opts += -no-feature-syntaxhighlighter
-#$(package)_config_opts += -no-feature-textbrowser # Needed
-$(package)_config_opts += -no-feature-textmarkdownwriter
-$(package)_config_opts += -no-feature-textodfwriter
-$(package)_config_opts += -no-feature-topleveldomain
-#$(package)_config_opts += -no-feature-udpsocket # Neede
-$(package)_config_opts += -no-feature-undocommand
-$(package)_config_opts += -no-feature-undogroup
-$(package)_config_opts += -no-feature-undostack
-$(package)_config_opts += -no-feature-undoview
-$(package)_config_opts += -no-feature-vnc
-#$(package)_config_opts += -no-feature-wizard # Needed
-$(package)_config_opts += -no-feature-fontconfig
-
 $(package)_config_opts += -no-xcb-xlib
-$(package)_config_opts += -no-feature-xlib
-$(package)_config_opts += -no-feature-xkbcommon
+$(package)_config_opts += -no-zstd
 
-# https://bugreports.qt.io/browse/QTBUG-99957
-$(package)_config_opts += -no-pch
-
-$(package)_config_opts += -no-opengl
-$(package)_config_opts += -no-feature-vulkan
 ifneq ($(LTO),)
 $(package)_config_opts += -ltcg
 endif
-$(package)_config_opts += -platform linux-g++ -xplatform linux-g++
-ifneq (,$(findstring -stdlib=libc++,$($(1)_cxx)))
-$(package)_config_opts_x86_64 = -xplatform linux-clang-libc++
-endif
-
 endef
 
 define $(package)_fetch_cmds
@@ -158,8 +149,7 @@ define $(package)_preprocess_cmds
   patch -p1 -i $($(package)_patch_dir)/qtbase-moc-ignore-gcc-macro.patch && \
   patch -p1 -i $($(package)_patch_dir)/rcc_hardcode_timestamp.patch && \
   patch -p1 -i $($(package)_patch_dir)/fast_fixed_dtoa_no_optimize.patch && \
-  patch -p1 -i $($(package)_patch_dir)/guix_cross_lib_path.patch && \
-  patch -p1 -i $($(package)_patch_dir)/no_pthread_cond_clockwait.patch
+  patch -p1 -i $($(package)_patch_dir)/guix_cross_lib_path.patch
 endef
 
 define $(package)_config_cmds
