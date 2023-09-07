a1038f9571b0827ba9d845c543132a71dcf74dcf
tobtoht <tob@featherwallet.org>
2023-09-07 09:00:27 +0200

    depends: libzip: update to 1.10.1

--- contrib/depends/packages/libzip.mk
+++ contrib/depends/packages/libzip.mk
@@ -1,8 +1,8 @@
 package=libzip
-$(package)_version=1.10.0
+$(package)_version=1.10.1
 $(package)_download_path=https://libzip.org/download/
-$(package)_file_name=libzip-1.10.0.tar.xz
-$(package)_sha256_hash=cd2a7ac9f1fb5bfa6218272d9929955dc7237515bba6e14b5ad0e1d1e2212b43
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=dc3c8d5b4c8bbd09626864f6bcf93de701540f761d76b85d7c7d710f4bd90318
 $(package)_dependencies=zlib
 $(package)_patches += no-clonefile.patch
 
