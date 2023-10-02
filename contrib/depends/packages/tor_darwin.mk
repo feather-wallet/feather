88d79cc3f094161d2cae8f44d5e435c4312a735f
tobtoht <tob@featherwallet.org>
2023-10-02 16:30:32 +0200

    depends: tor_{mingw32,darwin}: update to 0.4.7.15

--- contrib/depends/packages/tor_darwin.mk
+++ contrib/depends/packages/tor_darwin.mk
@@ -1,8 +1,8 @@
 package=tor_darwin
-$(package)_version=0.4.7.13
-$(package)_download_path=https://dist.torproject.org/torbrowser/12.0.2/
-$(package)_file_name=TorBrowser-12.0.2-macos_ALL.dmg
-$(package)_sha256_hash=c6968a7041890de6ac344e9163ce0a1c0fb82394a2da9d4e7f77e0bce7a1c952
+$(package)_version=0.4.7.15
+$(package)_download_path=https://dist.torproject.org/torbrowser/12.5.6/
+$(package)_file_name=TorBrowser-12.5.6-macos_ALL.dmg
+$(package)_sha256_hash=8ba5b760e9ad959b74a056d614a9f618e3c783af30a6587b5a8194ec0baa4cea
 $(package)_extract_cmds=mkdir -p $$($(1)_extract_dir) && \
 	echo "$$($(1)_sha256_hash)  $$($(1)_source)" > $$($(1)_extract_dir)/.$$($(1)_file_name).hash && \
 	$(build_SHA256SUM) -c $$($(1)_extract_dir)/.$$($(1)_file_name).hash && \
--- contrib/depends/packages/tor_mingw32.mk
+++ contrib/depends/packages/tor_mingw32.mk
@@ -1,8 +1,8 @@
 package=tor_mingw32
-$(package)_version=0.4.7.13
-$(package)_download_path=https://dist.torproject.org/torbrowser/12.0.2/
-$(package)_file_name=tor-expert-bundle-12.0.2-windows-x86_64.tar.gz
-$(package)_sha256_hash=964f23d91b0a2339f655832f5787d7a3c1219f5062481c953c55cc6f302339c9
+$(package)_version=0.4.7.15
+$(package)_download_path=https://dist.torproject.org/torbrowser/12.5.6/
+$(package)_file_name=tor-expert-bundle-12.5.6-windows-x86_64.tar.gz
+$(package)_sha256_hash=0db0f8fc6c60fa62b8159468fbb476acf0dec2f8dcf1b9fe7db3b91538334461
 
 define $(package)_stage_cmds
     mkdir -p $($(package)_staging_prefix_dir)/Tor/ && \
