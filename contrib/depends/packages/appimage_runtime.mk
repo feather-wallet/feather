4028cc65c47568da6174a0957ea5a314cb0cf5c8
tobtoht <tob@featherwallet.org>
2023-10-16 16:02:13 +0200

    depends: libfuse 3.16.2, latest appimage runtime

--- contrib/depends/packages/appimage_runtime.mk
+++ contrib/depends/packages/appimage_runtime.mk
@@ -1,8 +1,8 @@
 package=appimage_runtime
-$(package)_version=c1ea7509bc179a05d907baca64f41875662f35f2
+$(package)_version=c9553b05938b22849ac3255ac923bf8e775ce539
 $(package)_download_path=https://github.com/AppImage/type2-runtime/archive/
 $(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=a7906c7d1610eacb6c67a16c554fac8875b05424fea49e291311c3e5db2237a3
+$(package)_sha256_hash=4a27451013b571cf9f5a13660719d091cc79f2344aafa2e48578ddc0e4618af1
 $(package)_dependencies=libsquashfuse zstd
 $(package)_patches=depends-fix.patch
 
@@ -13,12 +13,11 @@ endef
 define $(package)_build_cmds
     cd src/runtime && \
     export host_prefix="$(host_prefix)" && \
-    $(MAKE) runtime-fuse2 -e CC=$($(package)_cc) LDLAGS="$($(package)_ldflags)" && \
-    "${HOST}-strip" runtime-fuse2 && \
-    echo -ne 'AI\x02' | dd of=runtime-fuse2 bs=1 count=3 seek=8 conv=notrunc
+    $(MAKE) runtime-fuse3 -e CC=$($(package)_cc) LDLAGS="$($(package)_ldflags)" && \
+    "${HOST}-strip" runtime-fuse3
 endef
 
 define $(package)_stage_cmds
     cd src/runtime && \
-    cp -a runtime-fuse2 $($(package)_staging_prefix_dir)/runtime
+    cp -a runtime-fuse3 $($(package)_staging_prefix_dir)/runtime
 endef
