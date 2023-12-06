5b10ed3ca0ab062e476340d19e2607bc31c2f4e8
tobtoht <tob@featherwallet.org>
2023-12-06 22:44:13 +0100

    depends: tor_{darwin,mingw32}: update broken links

--- contrib/depends/packages/tor_darwin.mk
+++ contrib/depends/packages/tor_darwin.mk
@@ -1,8 +1,8 @@
 package=tor_darwin
 $(package)_version=0.4.8.9
-$(package)_download_path=https://dist.torproject.org/torbrowser/13.0.5/
-$(package)_file_name=tor-browser-macos-13.0.5.dmg
-$(package)_sha256_hash=c5c7efda8213f2647ed092d5cc19723343e3564d632bcf4139c23257fec456aa
+$(package)_download_path=https://dist.torproject.org/torbrowser/13.0.6/
+$(package)_file_name=tor-browser-macos-13.0.6.dmg
+$(package)_sha256_hash=c77af1f2cc1985980dcdac260bff34184d73cb08efff3f1f9fe6db84c5467de2
 $(package)_extract_cmds=mkdir -p $$($(1)_extract_dir) && \
 	echo "$$($(1)_sha256_hash)  $$($(1)_source)" > $$($(1)_extract_dir)/.$$($(1)_file_name).hash && \
 	$(build_SHA256SUM) -c $$($(1)_extract_dir)/.$$($(1)_file_name).hash && \
--- contrib/depends/packages/tor_mingw32.mk
+++ contrib/depends/packages/tor_mingw32.mk
@@ -1,7 +1,7 @@
 package=tor_mingw32
 $(package)_version=0.4.8.9
-$(package)_download_path=https://dist.torproject.org/torbrowser/13.0.5/
-$(package)_file_name=tor-expert-bundle-windows-x86_64-13.0.5.tar.gz
+$(package)_download_path=https://dist.torproject.org/torbrowser/13.0.6/
+$(package)_file_name=tor-expert-bundle-windows-x86_64-13.0.6.tar.gz
 $(package)_sha256_hash=e3b885da9a53c3c734ddf1683deb520d545f899748d8f53f143d3a545a2ef1e9
 
 define $(package)_stage_cmds
