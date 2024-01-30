6f8a3d0e5aeb321cc82c5e101f66985607749e13
tobtoht <tob@featherwallet.org>
2024-01-30 11:49:20 +0100

    depends: zlib: update to 1.3.1

--- contrib/depends/packages/zlib.mk
+++ contrib/depends/packages/zlib.mk
@@ -1,8 +1,8 @@
 package=zlib
-$(package)_version=1.3
+$(package)_version=1.3.1
 $(package)_download_path=https://github.com/madler/zlib/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=8a9ba2898e1d0d774eca6ba5b4627a11e5588ba85c8851336eb38de4683050a7
+$(package)_sha256_hash=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
 
 define $(package)_config_cmds
     CC="$($(package)_cc)" \
