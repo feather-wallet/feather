96c0b55ac8362b103fb0995b9ee65fc8efb8cb11
tobtoht <tob@featherwallet.org>
2024-02-01 02:51:08 +0100

    depends: libusb: update to 1.0.27

--- contrib/depends/packages/libusb.mk
+++ contrib/depends/packages/libusb.mk
@@ -1,8 +1,8 @@
 package=libusb
-$(package)_version=1.0.26
+$(package)_version=1.0.27
 $(package)_download_path=https://github.com/libusb/libusb/releases/download/v$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5
+$(package)_sha256_hash=ffaa41d741a8a3bee244ac8e54a72ea05bf2879663c098c82fc5757853441575
 
 define $(package)_preprocess_cmds
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
