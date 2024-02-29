2c6c08df9293a6d77919912df9f729fb4ca23107
tobtoht <tob@featherwallet.org>
2024-02-29 22:12:36 +0100

    depends: expat: update to 2.6.1

--- contrib/depends/packages/expat.mk
+++ contrib/depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.6.0
+$(package)_version=2.6.1
 $(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$($(package)_version))/
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=ff60e6a6b6ce570ae012dc7b73169c7fdf4b6bf08c12ed0ec6f55736b78d85ba
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=0c00d2760ad12efef6e26efc8b363c8eb28eb8c8de719e46d5bb67b40ba904a3
 
 # -D_DEFAULT_SOURCE defines __USE_MISC, which exposes additional
 # definitions in endian.h, which are required for a working
