8e991f21b11819c57c93991bfcf1d94cc3324828
tobtoht <tob@featherwallet.org>
2024-08-15 22:24:39 +0200

    flatpak: fix zoneinfo

--- contrib/flatpak/make_runtime.sh
+++ contrib/flatpak/make_runtime.sh
@@ -6,6 +6,10 @@ cd empty
 
 mkdir export files usr
 
+# Needs to exists, otherwise flatpak doesn't mount zoneinfo
+# https://github.com/flatpak/flatpak/blob/8b4f523c4f8287d57f1a84a3a8216efe200c5fbf/common/flatpak-run.c#L1605
+mkdir -p usr/share/zoneinfo
+
 cat << EOF > metadata
 [Runtime]
 name=org.featherwallet.Empty
