249e31f5fabc4065998715571e7f0d4e1b78df57
tobtoht <tob@featherwallet.org>
2024-09-25 00:47:08 +0200

    depends: utf8proc: update to 2.9.0

--- contrib/depends/packages/utf8proc.mk
+++ contrib/depends/packages/utf8proc.mk
@@ -1,28 +1,23 @@
 package=utf8proc
-$(package)_version=2.8.0
-$(package)_download_path=https://github.com/JuliaStrings/utf8proc/archive/refs/tags/
-$(package)_download_file=v$($(package)_version).tar.gz
+$(package)_version=2.9.0
+$(package)_download_path=https://github.com/JuliaStrings/utf8proc/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=a0a60a79fe6f6d54e7d411facbfcc867a6e198608f2cd992490e46f04b1bcecc
+$(package)_sha256_hash=bd215d04313b5bc42c1abedbcb0a6574667e31acee1085543a232204e36384c4
 $(package)_patches=force_static.patch
+$(package)_build_subdir=build
 
 define $(package)_preprocess_cmds
   patch -p1 < $($(package)_patch_dir)/force_static.patch
 endef
 
 define $(package)_config_cmds
-    echo "$($(package)_cmake)" && \
-    mkdir build && \
-    cd build && \
-    $($(package)_cmake) ..
+  $($(package)_cmake) -S .. -B .
 endef
 
 define $(package)_build_cmds
-    cd build && \
-    $(MAKE)
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-    cd build && \
-    $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
