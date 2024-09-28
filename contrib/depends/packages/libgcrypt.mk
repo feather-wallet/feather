0634845cb9456faeac23a04c2e98665af0ccac94
tobtoht <tob@featherwallet.org>
2024-09-28 19:20:34 +0200

    depends: libgcrypt: update to 1.11.0

--- contrib/depends/packages/libgcrypt.mk
+++ contrib/depends/packages/libgcrypt.mk
@@ -1,8 +1,8 @@
 package=libgcrypt
-$(package)_version=1.10.3
+$(package)_version=1.11.0
 $(package)_download_path=https://www.gnupg.org/ftp/gcrypt/libgcrypt/
-$(package)_file_name=libgcrypt-$($(package)_version).tar.bz2
-$(package)_sha256_hash=8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa
+$(package)_file_name=libgcrypt-$($(package)_version).tar.gz
+$(package)_sha256_hash=2382891207d3b000b20c81dbf2036516a535d31abd80f57d455e711e1dde5ff5
 $(package)_dependencies=libgpg-error
 
 define $(package)_set_vars
@@ -13,16 +13,9 @@ define $(package)_preprocess_cmds
     cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
 endef
 
-# TODO: building on linux with $($(package)_autoconf) fails for mysterious reasons
-ifeq ($(host_os),linux)
-define $(package)_config_cmds
-    CFLAGS='-fPIE' CXXFLAGS='-fPIE' ./configure --host=$(host) --enable-digests="sha256 blake2" --enable-ciphers="aes chacha20" --disable-amd64-as-feature-detection --disable-asm --disable-avx-support --disable-avx2-support --disable-sse41-support --disable-shared --enable-static --disable-doc --with-libgpg-error-prefix=$(host_prefix) --prefix=$(host_prefix)
-endef
-else
 define $(package)_config_cmds
     $($(package)_autoconf) --disable-shared --enable-static --disable-doc --disable-asm --with-libgpg-error-prefix=$(host_prefix)
 endef
-endif
 
 define $(package)_build_cmds
     $(MAKE) $($(package)_build_opts)
