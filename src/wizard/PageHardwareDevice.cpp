cbbc9526a59c0663480571a8ee8ac50bd8e78819
tobtoht <tob@featherwallet.org>
2024-09-29 14:28:56 +0200

    wizard: don't enumerate supported devices

--- src/wizard/PageHardwareDevice.cpp
+++ src/wizard/PageHardwareDevice.cpp
@@ -16,8 +16,8 @@ PageHardwareDevice::PageHardwareDevice(WizardFields *fields, QWidget *parent)
 {
     ui->setupUi(this);
 
-    ui->combo_deviceType->addItem("Ledger Nano S (PLUS) / X", DeviceType::LEDGER);
-    ui->combo_deviceType->addItem("Trezor Model T / Safe 3", DeviceType::TREZOR);
+    ui->combo_deviceType->addItem("Ledger", DeviceType::LEDGER);
+    ui->combo_deviceType->addItem("Trezor", DeviceType::TREZOR);
 
     connect(ui->btnOptions, &QPushButton::clicked, this, &PageHardwareDevice::onOptionsClicked);
 }
