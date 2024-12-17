5a5602e28830167bed52c2d8666b6ae6bdd2d135
tobtoht <tob@featherwallet.org>
2024-12-17 22:34:07 +0100

    depends: libXau: update to 1.0.12

--- contrib/depends/packages/libXau.mk
+++ contrib/depends/packages/libXau.mk
@@ -1,34 +1,33 @@
 package=libXau
-$(package)_version=1.0.11
+$(package)_version=1.0.12
 $(package)_download_path=https://xorg.freedesktop.org/releases/individual/lib/
-$(package)_file_name=libXau-$($(package)_version).tar.xz
-$(package)_sha256_hash=f3fa3282f5570c3f6bd620244438dbfbdd580fc80f02f549587a0f8ab329bbeb
+$(package)_file_name=libXau-$($(package)_version).tar.gz
+$(package)_sha256_hash=2402dd938da4d0a332349ab3d3586606175e19cb32cb9fe013c19f1dc922dcee
 $(package)_dependencies=xorgproto
-
-# When updating this package, check the default value of
-# --disable-xthreads. It is currently enabled.
-define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-lint-library --without-lint
-  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
-  $(package)_config_opts += --with-pic
-endef
+$(package)_patches=toolchain.txt
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  rm Makefile.in aclocal.m4 compile config.guess config.h.in config.sub configure depcomp \
+     install-sh ltmain.sh missing test-driver man/Makefile.in && \
+  rm -rf m4 && \
+  cp $($(package)_patch_dir)/toolchain.txt toolchain.txt && \
+  sed -i -e 's|@host_prefix@|$(host_prefix)|' \
+         -e 's|@cc@|$($(package)_cc)|' \
+         -e 's|@cxx@|$($(package)_cxx)|' \
+         -e 's|@ar@|$($(package)_ar)|' \
+         -e 's|@strip@|$(host_STRIP)|' \
+         -e 's|@arch@|$(host_arch)|' \
+	  toolchain.txt
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  meson setup --cross-file toolchain.txt build
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  ninja -C build
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  rm -rf share lib/*.la
+  DESTDIR=$($(package)_staging_dir) ninja -C build install
 endef
