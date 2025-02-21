078bcc29bd1efb91497f057f223809cf481da8f5
tobtoht <tob@featherwallet.org>
2025-02-21 19:23:49 +0100

    depends: zstd: update to 1.5.7

--- contrib/depends/packages/zstd.mk
+++ contrib/depends/packages/zstd.mk
@@ -1,8 +1,12 @@
 package=zstd
-$(package)_version=1.5.6
+$(package)_version=1.5.7
 $(package)_download_path=https://github.com/facebook/zstd/releases/download/v$($(package)_version)
 $(package)_file_name=zstd-$($(package)_version).tar.gz
-$(package)_sha256_hash=8c29e06cf42aacc1eafc4077ae2ec6c6fcb96a626157e0593d5e82a34fd403c1
+$(package)_sha256_hash=eb33e51f49a15e023950cd7825ca74a4a2b43db8354825ac24fc1b7ee09e6fa3
+
+define $(package)_preprocess_cmds
+  rm -rf tests doc contrib/pzstd/images build/single_file_libs/examples
+endef
 
 define $(package)_build_cmds
   $($(package)_cmake) -DCMAKE_INSTALL_PREFIX=$(host_prefix) -DHOST=$(host) -DZSTD_LEGACY_SUPPORT=OFF -B build-cmake-debug -S build/cmake && \
