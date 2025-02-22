fad3eb259cf795d114c5401be8e957f28bb3d872
tobtoht <tob@featherwallet.org>
2025-02-22 09:10:29 +0100

    depends: libusb: build with cmake

--- contrib/depends/packages/hidapi.mk
+++ contrib/depends/packages/hidapi.mk
@@ -4,9 +4,14 @@ $(package)_download_path=https://github.com/libusb/hidapi/archive/refs/tags
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=a5714234abe6e1f53647dd8cba7d69f65f71c558b7896ed218864ffcf405bcbd
 $(package)_linux_dependencies=libusb eudev
+$(package)_patches=cmake-fix-libusb.patch
 
 define $(package)_set_vars
   $(package)_config_opts+=-DBUILD_SHARED_LIBS=OFF
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/cmake-fix-libusb.patch
 endef
 
 define $(package)_config_cmds
--- contrib/depends/packages/libusb.mk
+++ contrib/depends/packages/libusb.mk
@@ -4,23 +4,20 @@ $(package)_download_path=https://github.com/libusb/libusb/archive/refs/tags
 $(package)_download_file=v$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=e8f18a7a36ecbb11fb820bd71540350d8f61bcd9db0d2e8c18a6fb80b214a3de
-$(package)_patches=fix-c11-check.patch
+$(package)_patches=CMakeLists.txt config.h.in
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/fix-c11-check.patch && \
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
-  autoreconf -i
+  cp -f $($(package)_patch_dir)/CMakeLists.txt . && \
+  cp -f $($(package)_patch_dir)/config.h.in .
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
-  $(package)_config_opts_linux=--with-pic --disable-udev
-  $(package)_config_opts_mingw32=--disable-udev
-  $(package)_config_opts_darwin=--disable-udev
+  $(package)_config_opts := -DBUILD_SHARED_LIBS=OFF
+  $(package)_config_opts_linux += -DLIBUSB_ENABLE_UDEV=OFF
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  $($(package)_cmake) .
 endef
 
 define $(package)_build_cmd
@@ -29,8 +26,4 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  cp -f lib/libusb-1.0.a lib/libusb.a
 endef
