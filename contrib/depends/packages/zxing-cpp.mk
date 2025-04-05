ffa1c427b1b64e21a0fa970164f9dffb2cc743a1
tobtoht <tob@featherwallet.org>
2025-04-05 14:10:49 +0200

    depends: zxing-cpp: update to 2.3.0

--- contrib/depends/packages/zxing-cpp.mk
+++ contrib/depends/packages/zxing-cpp.mk
@@ -1,12 +1,16 @@
 package=zxing-cpp
-$(package)_version=2.2.1
+$(package)_version=2.3.0
 $(package)_download_path=https://github.com/$(package)/$(package)/archive/refs/tags
 $(package)_download_file=v$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=02078ae15f19f9d423a441f205b1d1bee32349ddda7467e2c84e8f08876f8635
+$(package)_sha256_hash=64e4139103fdbc57752698ee15b5f0b0f7af9a0331ecbdc492047e0772c417ba
 
 define $(package)_set_vars
   $(package)_config_opts=-DBUILD_WRITERS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF
+endef
+
+define $(package)_preprocess_cmds
+  rm -rf test wrappers
 endef
 
 define $(package)_config_cmds
