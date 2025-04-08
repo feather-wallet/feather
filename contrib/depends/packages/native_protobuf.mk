a3ce97131217bb57503c3f2a9b2a6ebf395c31fc
tobtoht <tob@featherwallet.org>
2025-04-08 18:54:08 +0200

    depends: protobuf: update to 30.2

--- contrib/depends/packages/native_protobuf.mk
+++ contrib/depends/packages/native_protobuf.mk
@@ -1,8 +1,8 @@
 package=native_protobuf
-$(package)_version=29.1
+$(package)_version=30.2
 $(package)_download_path=https://github.com/protocolbuffers/protobuf/releases/download/v$($(package)_version)
 $(package)_file_name=protobuf-$($(package)_version).tar.gz
-$(package)_sha256_hash=3d32940e975c4ad9b8ba69640e78f5527075bae33ca2890275bf26b853c0962c
+$(package)_sha256_hash=fb06709acc393cc36f87c251bb28a5500a2e12936d4346099f2c6240f6c7a941
 $(package)_dependencies=native_abseil
 
 define $(package)_set_vars
