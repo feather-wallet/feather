d7ed3795b38c08c2afb6443e6b16c2b3b8da3c16
tobtoht <tob@featherwallet.org>
2025-07-19 21:06:28 +0200

    depends: libsquashfuse: fix arm-linux-gnueabihf build

--- contrib/depends/packages/libsquashfuse.mk
+++ contrib/depends/packages/libsquashfuse.mk
@@ -1,12 +1,12 @@
 package=libsquashfuse
-$(package)_version=0.5.2
+$(package)_version=0.6.1
 $(package)_download_path=https://github.com/vasi/squashfuse/releases/download/$($(package)_version)
 $(package)_file_name=squashfuse-$($(package)_version).tar.gz
-$(package)_sha256_hash=54e4baaa20796e86a214a1f62bab07c7c361fb7a598375576d585712691178f5
+$(package)_sha256_hash=7b18a58c40a3161b5c329ae925b72336b5316941f906b446b8ed6c5a90989f8c
 $(package)_dependencies=libfuse zstd
 
 define $(package)_config_cmds
-  $($(package)_autoconf) --with-zstd=$(host_prefix) --without-zlib CFLAGS=-no-pie LDFLAGS=-static
+  $($(package)_autoconf) --with-zstd=$(host_prefix) --without-zlib CFLAGS="-no-pie -Wno-error=incompatible-pointer-types" LDFLAGS=-static
 endef
 
 define $(package)_build_cmds
