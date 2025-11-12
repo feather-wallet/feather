99770053467c9d178add0181c4ab1668d4ef034b
tobtoht <tob@featherwallet.org>
2025-11-12 12:13:07 +0100

    depends: tor_darwin: update to 0.4.8.19

--- contrib/depends/packages/tor_darwin.mk
+++ contrib/depends/packages/tor_darwin.mk
@@ -1,8 +1,8 @@
 package=tor_darwin
-$(package)_version=0.4.8.16
-$(package)_download_path=https://dist.torproject.org/torbrowser/14.0.9/
-$(package)_file_name=tor-browser-macos-14.0.9.dmg
-$(package)_sha256_hash=82a3dba862a34af1796e6a032f08c591e88cbac5831bedc83e168c1833d62506
+$(package)_version=0.4.8.19
+$(package)_download_path=https://archive.torproject.org/tor-package-archive/torbrowser/15.0/
+$(package)_file_name=tor-browser-macos-15.0.dmg
+$(package)_sha256_hash=d297c9b5e07496020051346383a4053ddf4da5ba426e41ac48c2e1e3e870e819
 $(package)_extract_cmds=mkdir -p $$($(1)_extract_dir) && \
 	echo "$$($(1)_sha256_hash)  $$($(1)_source)" > $$($(1)_extract_dir)/.$$($(1)_file_name).hash && \
 	$(build_SHA256SUM) -c $$($(1)_extract_dir)/.$$($(1)_file_name).hash && \
