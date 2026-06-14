0b43c521df8d7b1201ffc6f16eab27e74784db11
tobtoht <tob@featherwallet.org>
2026-06-14 21:06:11 +0200

    depends: abseil: update to 20260526.0

--- contrib/depends/packages/native_abseil.mk
+++ contrib/depends/packages/native_abseil.mk
@@ -1,8 +1,8 @@
 package=native_abseil
-$(package)_version=20260107.1
+$(package)_version=20260526.0
 $(package)_download_path=https://github.com/abseil/abseil-cpp/releases/download/$($(package)_version)/
 $(package)_file_name=abseil-cpp-$($(package)_version).tar.gz
-$(package)_sha256_hash=4314e2a7cbac89cac25a2f2322870f343d81579756ceff7f431803c2c9090195
+$(package)_sha256_hash=6e1aee535473414164bf83e4ebc40240dec71a4701f8a642d906e95bea1aea0c
 
 define $(package)_config_cmds
   $($(package)_cmake)
